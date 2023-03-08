a06e0d93e8e91fe12f50b72dcd15c83570d6e738
grayfox <null@grayfox.dev>
2023-03-07 20:57:35 -0500

    Refactored Sample 2

---
+++ src/heavy-metal/02-argbuffers-redux/common.hpp
@@ -0,0 +1,20 @@
+#ifndef NS_PRIVATE_IMPLEMENTATION
+#define NS_PRIVATE_IMPLEMENTATION
+#endif
+
+#ifndef MTL_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+#endif
+
+#ifndef MTK_PRIVATE_IMPLEMENTATION
+#define MTK_PRIVATE_IMPLEMENTATION
+#endif
+
+#ifndef CA_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+#endif
+
+#include <Metal/Metal.hpp>
+#include <AppKit/AppKit.hpp>
+#include <MetalKit/MetalKit.hpp>
+
